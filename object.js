8ebfa221c3b7d560ef27606c13c3ce3b9c8f5344
RUMON <rezviahmedrumon@gmail.com>
2024-02-01 13:48:15 +0600

    added object class

---
+++ object.js
@@ -0,0 +1,30 @@
+// const age = 21;
+// const school = 'golapganj';
+// const isPassed = true;
+// // let isDeveloper;
+// const subjects = ['bangla', 'english', 'physics', 'math']
+
+// console.log(isDeveloper);
+
+
+//object
+//non primitive
+
+const bottle = {
+    brand: 'Mum',
+    price: 40,
+    color: 'white',
+    isClean: false,
+}
+
+
+const subject = {
+   name: 'biology',
+   teacher: 'rumon',
+   examDate: '30 Feb',
+   chapters: ['first', 'second', 'third'],
+   exam:{
+    name:'Final Exam',
+    marks: 100,
+   }
+}
