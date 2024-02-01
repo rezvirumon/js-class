ac330c53892a888b68ae9def67825c4f546e6723
RUMON <rezviahmedrumon@gmail.com>
2024-02-01 13:34:11 +0600

    added reverse class

---
+++ reverse.js
@@ -0,0 +1,29 @@
+// const sentence = 'i want to learning web dev.';
+
+// //const result = reverse word;
+
+
+// // 
+// let reverse = ''
+// for (const letter of sentence) {
+//     // console.log(letter);
+//     reverse = letter + reverse;
+
+// }
+
+// // console.log(reverse)
+
+// const rev = '';
+// for(let i = 0; i <sentence.length; i++){
+//     // console.log(i);
+//     // console.log(sentence[i]);
+//     const letter = sentence[i];
+//     rev = letter + rev;
+// }
+
+// // console.log(rev);
+
+
+// //shortcut
+// const reversed = sentence.split('').reverse();
+// console.log(reversed);
