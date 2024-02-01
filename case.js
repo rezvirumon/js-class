1d7da23d6b4a1e73dbcf6fd658000b2b1bdcb009
RUMON <rezviahmedrumon@gmail.com>
2024-02-01 12:52:46 +0600

    added string compare

---
+++ case.js
@@ -0,0 +1,33 @@
+const school = "GOLAPGANJ SCHOOL";
+//compare to string
+// console.log(school)
+// console.log(school.toLowerCase());
+// console.log(school.toUpperCase());
+
+
+
+const subject = 'Chemistry' //upppercase
+const book = 'chemistry'; //lowercase
+
+
+
+// if (subject.toLowerCase === book.toLowerCase) {
+//     console.log('i am reading book this exam i will be try my best');
+
+// }
+// else {
+//     console.log('hudai hudai porte boshsi');
+// }
+    
+
+
+const drink = 'water';
+const liquid = 'water  ';
+
+if(drink.trim() === liquid.trim()){
+    console.log('pani khamu');
+}
+
+else{
+    console.log('pani khamuna');
+}
---
+++ string.js
@@ -0,0 +1,15 @@
+const name = `rumon`;
+
+
+const numbers = [10, 15, 20, 25, 30, 35];
+// console.log(numbers.length); //array length
+// console.log(numbers[1]); //array tergat..
+
+
+//string is immutable -------> not changable
+//array is mutable ----------> youu can change the array elements
+const capital = 'Dhaka';
+// console.log(capital.length);
+// console.log(capital[0]);
+numbers[5] = 11; //change arry numbers
+console.log(capital); 
