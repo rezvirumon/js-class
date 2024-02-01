044bf8e8ddb6cc073f70da0924add53d467dad81
RUMON <rezviahmedrumon@gmail.com>
2024-02-01 13:15:44 +0600

    added more js classes

---
+++ concat.js
@@ -0,0 +1,15 @@
+const first = 'Rezvi';
+const last = 'Ahmed';
+
+// const fullName = first + last;
+const fullName = first + ' ' + last;
+// console.log(fullName);
+
+
+const fullName2 = first.concat(' ').concat(last);
+console.log(fullName2);
+
+
+//incluids
+
+console.log(last.includes('y'))
---
+++ slice.js
@@ -0,0 +1,23 @@
+// const address = 'golapganj';
+// const part = address.slice(2,5);
+
+
+// console.log(part);
+
+
+// const sentence =  'i am a good and hardworking person.';
+// // console.log(sentence.split(' '));
+// console.log(sentence.split('a'));
+
+const brotherS = 'rumon,mainul,nayeem,hashib';
+const brother  = brotherS.split(',');
+console.log(brother);
+const smallBrother = [ 'rumon', ' mainul', ' nayeem', ' hashib' ];
+console.log(smallBrother.join());
+console.log(smallBrother.join('|'));
+console.log(smallBrother.join('-'));
+
+
+
+
+
