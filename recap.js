121728c0b0cd2859adf894fc878b9cd06c27260c
RUMON <rezviahmedrumon@gmail.com>
2024-02-03 22:14:17 +0600

    add conditional-return class

---
+++ recap.js
@@ -0,0 +1,12 @@
+function doMath(num1, num2){
+    const sum = num1 + num2;
+    const diff = num1 - num2;
+    const multiply = sum * diff;
+    const result = multiply /2;
+    return result;
+}
+
+const result = doMath(10, 5);
+console.log(result);
+
+
