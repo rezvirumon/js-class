a6b8a00bed498127098d8955631820ce4bce3875
RUMON <rezviahmedrumon@gmail.com>
2024-02-06 22:06:31 +0600

    added math.js class

---
+++ recap/math.js
@@ -0,0 +1,29 @@
+
+// // const Math = {
+// //     min: function min(num1, num2){
+// //         return 5
+// //     }
+// // }
+
+// console.log(Math.max);
+// // const min = Math.min
+
+
+
+// const min = Math.min(5, 21, 56, 65, 99, 126);
+// console.log(min);
+const max = Math.max(5, 21, 56, 65, 99, 126);
+console.log(max);
+
+
+console.log(Math.PI)
+console.log(Math.abs(-88))
+console.log(Math.round(2.85))
+console.log(Math.floor(2.85))
+console.log(Math.ceil(2.85))
+
+
+console.log(Math.random())
+console.log(Math.random()*10)
+const rand = Math.round(Math.random()*10)
+console.log(rand)
