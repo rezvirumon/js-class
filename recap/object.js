904e8d515c387471df6fac3e0de057aa8d2784a4
RUMON <rezviahmedrumon@gmail.com>
2024-02-08 20:54:12 +0600

    add object bonus class js

---
+++ recap/object.js
@@ -0,0 +1,16 @@
+
+const clientInfo = {
+    name: 'Rumon',
+    id: 121,
+    address: 'Golapganj',
+    package: '8mbps',
+    mothlyBill: 500,
+    paymentHistory: [2020, 2021, 2023],
+    routerAccess: {
+        router1: 'golapganj',
+        router2: 'rangpur'
+    }
+}
+
+
+
